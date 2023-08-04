89ffbd1f20b094b04475a4a896402fdc4fb57577
Alfredo <alfredonzk@gmail.com>
2023-08-04 20:45:03 -0300

    api github

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,49 @@
+import { useEffect, useState } from "react";
 import { PostCard } from "./components/PostCard";
 import { Profile } from "./components/Profile";
 import { SearchBar } from "./components/SearchBar";
 import { PostsConainer } from "./styles";
+import { api } from "../../lib/axios";
+
+export interface PostsI {
+  title: string,
+  comments: number,
+  body: string,
+  created_at: string,
+  number: number,
+  html_url: string,
+  user: {
+    login: string
+  }
+}
 
 export function Home() {
+  const [posts, setPosts] = useState<PostsI[]>([])
+
+  async function loadGetPots(query: string = "") {
+    const response = await api.get(
+      `/search/issues?q=${query}%20repo:${'alfredo-pg'}/${'github-blog'}`
+    )
+
+    setPosts(response.data.items)
+  }
+
+  useEffect(() => {
+    loadGetPots()
+  }, [])
+
   return (
     <>
       <Profile />
       <SearchBar />
 
       <PostsConainer to="/post/1" >
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
-        <PostCard />
+        {posts.map((post) => (
+          <PostCard 
+            key={post.number}
+            post={post}
+          />
+        ))}
       </PostsConainer>
     </>
   )
